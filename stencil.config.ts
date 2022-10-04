6707f51cb0738b3325befa6d8763d4a9646ce807
Daniel Peña <toyeca@gmail.com>
2022-10-04 22:52:15 +0200

    Stencil and Tailwind working together via Postcss.

---
+++ stencil.config.ts
@@ -0,0 +1,28 @@
+import { Config } from '@stencil/core';
+import { postcss } from '@stencil-community/postcss';
+import autoprefixer from 'autoprefixer';
+import tailwind from 'tailwindcss';
+
+// https://stenciljs.com/docs/config
+
+export const config: Config = {
+  globalStyle: 'src/global/app.css',
+  globalScript: 'src/global/app.ts',
+  taskQueue: 'async',
+  outputTargets: [
+    {
+      type: 'www',
+      // comment the following line to disable service workers in production
+      serviceWorker: null,
+      baseUrl: 'https://myapp.local/',
+    },
+  ],
+  plugins: [
+    postcss({
+      plugins: [
+        autoprefixer(),
+        tailwind('./tailwind.config.js'),
+      ]
+    })
+  ]
+};
